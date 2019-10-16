a0cca8b138bb3c8522d21c0fa5b030ab2cbd2b78
chelsea148629 <chelsea148629@gmail.com>
2019-10-16 10:54:07 +0800

    delete sad face

--- src/main/java/exception/NoWordFoundException.java
+++ src/main/java/exception/NoWordFoundException.java
@@ -4,7 +4,7 @@ public class NoWordFoundException extends WordUpException {
     private String searchWord;
 
     public NoWordFoundException(String searchWord) {
-        super("     ☹ OOPS: The word you are searching is not in the word bank: ");
+        super("     OOPS: The word you are searching is not in the word bank: ");
         this.searchWord = searchWord;
     }
 
